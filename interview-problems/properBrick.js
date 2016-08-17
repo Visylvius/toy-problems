76c05c486b8cde282e59585f6fdc5577de8a87db
Loren Barrick <lorenbarrick@gmail.com>
2016-08-17 16:42:20 -0700

    add brick problem

---
+++ interview-problems/properBrick.js
@@ -0,0 +1,15 @@
+function brickLayer(bigBricks, smallBricks, total) {
+  this.bigBricks = bigBricks;
+  this.smallBricks = smallBricks;
+  this.total = total;
+}
+
+brickLayer.prototype.totalBricks = function(bigBricks, smallBricks, total) {
+
+};
+
+
+//small bricks are equal to 1
+//big bricks are equal to 5
+
+//give the total amount, do you have enough small/big bricks to complete the project ahead of you? 
