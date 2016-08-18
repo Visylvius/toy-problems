38c06792fb38e1b1737b992a776ddcb6bd79ab6c
Loren Barrick <lorenbarrick@gmail.com>
2016-08-18 08:09:39 -0700

    add file structure and readme

--- interview-problems/properBrick/properBrick.js
+++ interview-problems/properBrick/properBrick.js
@@ -9,10 +9,6 @@ brickLayer.prototype.totalBricks = function(bigBricks, smallBricks, total) {
 };
 
 
-//small bricks are equal to 1
-//big bricks are equal to 5
-
-//give the total amount, do you have enough small/big bricks to complete the project ahead of you?
 
 
 //first check the total versus the big bricks
