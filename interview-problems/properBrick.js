ea62b2a91aab95bd8ff0a6a704525f83cbe4e8b4
Loren Barrick <lorenbarrick@gmail.com>
2016-08-18 07:30:49 -0700

    add proper brick

--- interview-problems/properBrick.js
+++ interview-problems/properBrick.js
@@ -12,4 +12,10 @@ brickLayer.prototype.totalBricks = function(bigBricks, smallBricks, total) {
 //small bricks are equal to 1
 //big bricks are equal to 5
 
-//give the total amount, do you have enough small/big bricks to complete the project ahead of you? 
+//give the total amount, do you have enough small/big bricks to complete the project ahead of you?
+
+
+//first check the total versus the big bricks
+  //if big bricks is equal to total return true
+  //if big bricks is greater than the total
+    //add small bricks until big bricks + small bricks are equal to the total.
